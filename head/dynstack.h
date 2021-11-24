e94587096509d8e2f6737389074c5a139d6a9ab4
jedishao23 <shao724854691@163.com>
2021-11-24 11:14:18 +0800

    dynamic stack

--- head/dynstack.h
+++ head/dynstack.h
@@ -2,21 +2,16 @@
 
 typedef struct Stack
 {
-	void **base;
-	void **top;
-	int stacksize;
-}Stack;
+	void **base;   //栈底指针
+	void **top;	   //栈顶指针
+	int stacksize; //栈当前可使用的最大容量, 以元素个数计
+} Stack;
 
 Stack global_sym_stack, local_sym_stack;
 
 void stack_init(Stack *stack, int initsize);
-
-void* stack_push(Stack *stack, void *element, int size);
-
+void *stack_push(Stack *stack, void *element, int size);
 void stack_pop(Stack *stack);
-
-void* stack_get_top(Stack *stack);
-
+void *stack_get_top(Stack *stack);
 int stack_is_empty(Stack *stack);
-
 void stack_destroy(Stack *stack);
