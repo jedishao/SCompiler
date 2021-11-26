48599a3a4d4220fa00752950f9200990fb831ab2
jedishao23 <shao724854691@163.com>
2021-11-26 12:01:34 +0800

    symbol

--- head/symbol.h
+++ head/symbol.h
@@ -12,9 +12,16 @@ typedef struct Symbol
     int v;                   //符号的单词编码
     int r;                   //符号关联的寄存器
     int c;                   //符号关联值
-    Type type;        //符号数据类型
-    Symbol *next;     //关联的其它符号
-    Symbol *prev_tok; //指向前一定义的同名符号
+    Type type;               //符号数据类型
+    Symbol *next;            //关联的其它符号
+    Symbol *prev_tok;        //指向前一定义的同名符号
 }Symbol;
 
 Symbol *sym_direct_push(Stack *ss, int v, Type *type, int c);
+Symbol *sym_push(int v, Type *type, int r, int c);
+Symbol *func_sym_push(int v, Type *type);
+Symbol *var_sym_push(Type *type, int r, int v, int addr);
+Symbol *sec_sym_push(char *sec, int c);
+void sym_pop(Stack *ptop, Symbol *b);
+Symbol *struct_search(int v);
+Symbol *sym_search(int v);
--- src/symbol.c
+++ src/symbol.c
@@ -77,3 +77,93 @@ Symbol *func_sym_push(int v, Type *type)
     *ps = s;
     return s;
 }
+
+Symbol *var_sym_push(Type *type, int r, int v, int addr)
+{
+    Symbol *sym = NULL;
+    if ((r & SC_VALMASK) == SC_LOCAL)
+    {
+        sym = sym_push(v, type, r, addr);
+    }
+    else if (v && (r & SC_VALMASK) == SC_GLOBAL)
+    {
+        sym = sym_search(v);
+        if (sym)
+            error("%s redefine\n", ((TKWord *)tktable.data[v])->spelling);
+        else
+            sym = sym_push(v, type, r | SC_SYM, 0);
+    }
+    //else 字符串常量符号
+    return sym;
+}
+
+/*******************************************
+  * 功能: 将节名称放入全局符号表
+  * sec:  节名称
+  * c:    符号关联值
+ ********************************************/
+Symbol *sec_sym_push(char *sec, int c)
+{
+    TKWord *tp;
+    Symbol *s;
+    Type type;
+    type.t = T_INT;
+    tp = tkword_insert(sec);
+    token = tp->tkcode;
+    s = sym_push(token, &token, SC_GLOBAL, c);
+    return s;
+}
+
+/*******************************************
+  * 功能: 弹出栈中符号直到栈顶符号为'b'
+  * ptop: 符号栈栈顶
+  * b:    符号指针
+ ********************************************/
+void sym_pop(Stack *ptop, Symbol *b)
+{
+    Symbol *s, **ps;
+    TKWord *ts;
+    int v;
+
+    s = (Symbol *)stack_get_top(ptop);
+    while (s != b)
+    {
+        v = s->v;
+        // update sym_struct sym_identifier
+        if ((v & SC_STRUCT) || v < SC_ANOM)
+        {
+            ts = (TKWord *)tktable.data[(v & ~SC_STRUCT)];
+            if (v & SC_STRUCT)
+                ps = &ts->sym_struct;
+            else
+                ps = &ts->sym_identifier;
+            *ps = s->prev_tok;
+        }
+        stack_pop(ptop);
+        s = (Symbol *)stack_get_top(ptop);
+    }
+}
+
+/*******************************************
+  * 功能: 查找结构定义
+  * v:    符号编号
+ ********************************************/
+Symbol *struct_search(int v)
+{
+    if (v >= tktable.count)
+        return NULL;
+    else
+        return ((TKWord *)tktable.data[v])->sym_struct;
+}
+
+/*******************************************
+  * 功能: 查找标识符定义
+  * v:    符号编号
+ ********************************************/
+Symbol *sym_search(int v)
+{
+    if (v >= tktable.count)
+        return NULL;
+    else
+        return ((TKWord *)tktable.data[v])->sym_identifier;
+}
