8f8b98d841deae7e275824a696d280a66d99f3ea
jedishao23 <shao724854691@163.com>
2021-11-27 14:01:42 +0800

    type

--- head/type.h
+++ head/type.h
@@ -6,6 +6,10 @@ typedef struct Type
     struct Symbol *ref; //引用符号
 } Type;
 
+Type char_pointer_type, //字符串指针
+    int_type,           //int类型
+    default_func_type;  //缺省函数类型
+
 /*数据类型编码*/
 enum e_TypeCode
 {
@@ -24,13 +28,13 @@ enum e_TypeCode
 /*存储类型*/
 enum e_StorageClass
 {
-    SC_GLOBAL = 0x00f0,     //包括整型常量、字符常量、字符串常量、全局变量、函数定义
-    SC_LOCAL = 0x00f1,      //栈中变量
-    SC_LLOCAL = 0x00f2,     //寄存器溢出存放栈中
-    SC_CMP = 0x00f3,        //使用标志寄存器
-    SC_VALMASK = 0x00ff,    //存储类型掩码
-    SC_LVAL = 0x0100,       //左值
-    SC_SYM = 0x0200,        //符号
+    SC_GLOBAL = 0x00f0,  //包括整型常量、字符常量、字符串常量、全局变量、函数定义
+    SC_LOCAL = 0x00f1,   //栈中变量
+    SC_LLOCAL = 0x00f2,  //寄存器溢出存放栈中
+    SC_CMP = 0x00f3,     //使用标志寄存器
+    SC_VALMASK = 0x00ff, //存储类型掩码
+    SC_LVAL = 0x0100,    //左值
+    SC_SYM = 0x0200,     //符号
 
     SC_ANOM = 0x10000000,   //匿名符号
     SC_STRUCT = 0x20000000, //结构体符号
